7ad67b6408fd94faa76186a303d1d8d8e348ad6e
antonino14 <146857658+antonino14@users.noreply.github.com>
2025-01-03 11:21:59 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 define("myhost", "postgres");
 define("myuser", "antonino_ottina");
 define("mypsw", "Cermenate14@");
@@ -18,6 +17,7 @@ function close_pg_connection($db) {
 
 /* Funzione di autenticazione utente */
 function authenticateUser($requiredUserType) {
+    session_start();
     if (!isset($_SESSION['logged_in']) || $_SESSION['user_type'] !== $requiredUserType) {
         header("Location: login_bibliotecario.php");
         exit();
@@ -32,19 +32,20 @@ function login_lettore($email, $password) {
 
     $logged = null;
     $db = open_pg_connection();
-    try {
-        $sql = "SELECT cf_lettore FROM biblioteca.utente_lettore WHERE email = $1 AND password = $2";
-        $params = array($email, $password); // Assumiamo che la password sia già hashata
+    $sql = "SELECT cf_lettore, password FROM biblioteca.utente_lettore WHERE email = $1";
 
-        pg_prepare($db, "check_user_lettore", $sql);
-        $result = pg_execute($db, "check_user_lettore", $params);
+    $params = array($email);
 
-        if ($row = pg_fetch_assoc($result)) {
+    $result = pg_prepare($db, "check_user_lettore", $sql);
+    $result = pg_execute($db, "check_user_lettore", $params);
+
+    if ($row = pg_fetch_assoc($result)) {
+        if (password_verify($password, $row['password'])) {
             $logged = $row['cf_lettore'];
         }
-    } finally {
-        close_pg_connection($db);
     }
+
+    close_pg_connection($db);
     return $logged;
 }
 
@@ -56,19 +57,20 @@ function login_bibliotecario($email, $password) {
 
     $logged = null;
     $db = open_pg_connection();
-    try {
-        $sql = "SELECT email FROM biblioteca.utente_bibliotecario WHERE email = $1 AND password = $2";
-        $params = array($email, $password); // Assumiamo che la password sia già hashata
+    $sql = "SELECT email, password FROM biblioteca.utente_bibliotecario WHERE email = $1";
 
-        pg_prepare($db, "check_user_bibliotecario", $sql);
-        $result = pg_execute($db, "check_user_bibliotecario", $params);
+    $params = array($email);
 
-        if ($row = pg_fetch_assoc($result)) {
+    $result = pg_prepare($db, "check_user_bibliotecario", $sql);
+    $result = pg_execute($db, "check_user_bibliotecario", $params);
+
+    if ($row = pg_fetch_assoc($result)) {
+        if (password_verify($password, $row['password'])) {
             $logged = $row['email'];
         }
-    } finally {
-        close_pg_connection($db);
     }
+
+    close_pg_connection($db);
     return $logged;
 }
 
@@ -79,27 +81,30 @@ function cambia_password($email, $new_password) {
     }
 
     $db = open_pg_connection();
-    try {
-        // Controlla se l'utente è un lettore o un bibliotecario
-        $query = "SELECT 'lettore' AS tipo FROM biblioteca.utente_lettore WHERE email = $1
-                  UNION
-                  SELECT 'bibliotecario' AS tipo FROM biblioteca.utente_bibliotecario WHERE email = $1";
-        pg_prepare($db, "check_user_type", $query);
-        $result = pg_execute($db, "check_user_type", array($email));
 
-        if ($row = pg_fetch_assoc($result)) {
-            $query = $row['tipo'] == 'lettore' ?
-                "UPDATE biblioteca.utente_lettore SET password = $1 WHERE email = $2" :
-                "UPDATE biblioteca.utente_bibliotecario SET password = $1 WHERE email = $2";
+    // Controlla se l'utente è un lettore o un bibliotecario
+    $query = "SELECT 'lettore' AS tipo FROM biblioteca.utente_lettore WHERE email = $1
+              UNION
+              SELECT 'bibliotecario' AS tipo FROM biblioteca.utente_bibliotecario WHERE email = $1";
+    $result = pg_prepare($db, "check_user_type", $query);
+    $result = pg_execute($db, "check_user_type", array($email));
 
-            pg_prepare($db, "update_user", $query);
-            pg_execute($db, "update_user", array(md5($new_password), $email));
-            return true;
+    if ($row = pg_fetch_assoc($result)) {
+        $hashed_password = password_hash($new_password, PASSWORD_BCRYPT);
+        if ($row['tipo'] == 'lettore') {
+            $query = "UPDATE biblioteca.utente_lettore SET password = $1 WHERE email = $2";
+        } else {
+            $query = "UPDATE biblioteca.utente_bibliotecario SET password = $1 WHERE email = $2";
         }
-    } finally {
+
+        $result = pg_prepare($db, "update_user", $query);
+        $result = pg_execute($db, "update_user", array($hashed_password, $email));
+        
         close_pg_connection($db);
+        return $result;
+    } else {
+        close_pg_connection($db);
+        return false;
     }
-    return false;
 }
 ?>
-
