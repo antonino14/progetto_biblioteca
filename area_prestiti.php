fd9fd226187d6c6ab1e0bb9884c585eebe77153a
antonino14 <146857658+antonino14@users.noreply.github.com>
2025-01-03 11:17:14 +0100

    Update area_prestiti.php

--- area_prestiti.php
+++ area_prestiti.php
@@ -17,100 +17,96 @@
 <html>
 <head>
     <title>Area Prestiti</title>
-    <link rel="stylesheet" type="text/css" href="catalogo_styles.css">
+    <link rel="stylesheet" type="text/css" href="gestione_styles.css">
 </head>
 <body>
-    <div class="sidebar">
-        <h2>Menu</h2>
-        <ul>
-            <li><a href="catalogo.php">Catalogo</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-    </div>
-    <div class="container">
-        <header>
-            <h1>Prestiti Aperti</h1>
-        </header>
-        <main>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Codice Prestito</th>
-                        <th>Data Inizio</th>
-                        <th>Data Fine</th>
-                        <th>Stato</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    // Query per ottenere i prestiti aperti
-                    $query = "SELECT p.cod_prestito, p.data_inizio, p.data_fine, p.prestito_aperto AS stato
-                              FROM biblioteca.prestito p 
-                              JOIN biblioteca.lettore l ON p.lettore = l.CF
-                              WHERE l.CF = $1 AND p.prestito_aperto = TRUE
-                              ORDER BY p.data_fine ASC;";
-                    $result = pg_prepare($db, "prestiti_aperti", $query);
-                    $result = pg_execute($db, "prestiti_aperti", array($cf));
+    <?php include 'sidebar.php'; ?>
+    <div class="main-content">
+        <div class="container">
+            <header>
+                <h1>Prestiti Aperti</h1>
+            </header>
+            <main>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Codice Prestito</th>
+                            <th>Data Inizio</th>
+                            <th>Data Fine</th>
+                            <th>Stato</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Query per ottenere i prestiti aperti
+                        $query = "SELECT p.cod_prestito, p.data_inizio, p.data_fine, p.prestito_aperto AS stato
+                                  FROM biblioteca.prestito p 
+                                  JOIN biblioteca.lettore l ON p.lettore = l.CF
+                                  WHERE l.CF = $1 AND p.prestito_aperto = TRUE
+                                  ORDER BY p.data_fine ASC;";
+                        $result = pg_prepare($db, "prestiti_aperti", $query);
+                        $result = pg_execute($db, "prestiti_aperti", array($cf));
 
-                    // Popolamento della tabella con i libri
-                    while ($row = pg_fetch_assoc($result)) {
-                        echo "<tr>";
-                        echo "<td>{$row['cod_prestito']}</td>";
-                        echo "<td>{$row['data_inizio']}</td>";
-                        echo "<td>{$row['data_fine']}</td>";
-                        $stato = $row['stato'] == 't' ? 'In Corso' : 'Chiuso';
-                        echo "<td>{$stato}</td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </main>
-    </div>
-    <div class="container">
-        <header>
-            <h1>Prestiti Chiusi</h1>
-        </header>
-        <main>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Codice Prestito</th>
-                        <th>Data Inizio</th>
-                        <th>Data Fine</th>
-                        <th>Data Restituzione</th>
-                        <th>Consegnato</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    // Query per ottenere i prestiti chiusi
-                    $query = "SELECT p.cod_prestito, p.data_inizio, p.data_fine, p.data_restituzione
-                              FROM biblioteca.prestito p 
-                              JOIN biblioteca.lettore l ON p.lettore = l.CF
-                              WHERE l.CF = $1 AND p.prestito_aperto = FALSE
-                              ORDER BY p.data_fine ASC;";
-                    $result = pg_prepare($db, "prestiti_chiusi", $query);
-                    $result = pg_execute($db, "prestiti_chiusi", array($cf));
+                        // Popolamento della tabella con i libri
+                        while ($row = pg_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>{$row['cod_prestito']}</td>";
+                            echo "<td>{$row['data_inizio']}</td>";
+                            echo "<td>{$row['data_fine']}</td>";
+                            $stato = $row['stato'] == 't' ? 'In Corso' : 'Chiuso';
+                            echo "<td>{$stato}</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </main>
+        </div>
+        <div class="container">
+            <header>
+                <h1>Prestiti Chiusi</h1>
+            </header>
+            <main>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Codice Prestito</th>
+                            <th>Data Inizio</th>
+                            <th>Data Fine</th>
+                            <th>Data Restituzione</th>
+                            <th>Consegnato</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Query per ottenere i prestiti chiusi
+                        $query = "SELECT p.cod_prestito, p.data_inizio, p.data_fine, p.data_restituzione
+                                  FROM biblioteca.prestito p 
+                                  JOIN biblioteca.lettore l ON p.lettore = l.CF
+                                  WHERE l.CF = $1 AND p.prestito_aperto = FALSE
+                                  ORDER BY p.data_fine ASC;";
+                        $result = pg_prepare($db, "prestiti_chiusi", $query);
+                        $result = pg_execute($db, "prestiti_chiusi", array($cf));
 
-                    // Popolamento della tabella con i libri
-                    while ($row = pg_fetch_assoc($result)) {
-                        echo "<tr>";
-                        echo "<td>{$row['cod_prestito']}</td>";
-                        echo "<td>{$row['data_inizio']}</td>";
-                        echo "<td>{$row['data_fine']}</td>";
-                        echo "<td>{$row['data_restituzione']}</td>";
-                        $consegnato = $row['data_restituzione'] > $row['data_fine'] ? 'In Ritardo' : 'In Tempo';
-                        echo "<td>{$consegnato}</td>";
-                        echo "</tr>";
-                    }
+                        // Popolamento della tabella con i libri
+                        while ($row = pg_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>{$row['cod_prestito']}</td>";
+                            echo "<td>{$row['data_inizio']}</td>";
+                            echo "<td>{$row['data_fine']}</td>";
+                            echo "<td>{$row['data_restituzione']}</td>";
+                            $consegnato = $row['data_restituzione'] > $row['data_fine'] ? 'In Ritardo' : 'In Tempo';
+                            echo "<td>{$consegnato}</td>";
+                            echo "</tr>";
+                        }
 
-                    // Chiusura della connessione al database
-                    close_pg_connection($db);
-                    ?>
-                </tbody>
-            </table>
-        </main>
+                        // Chiusura della connessione al database
+                        close_pg_connection($db);
+                        ?>
+                    </tbody>
+                </table>
+            </main>
+        </div>
     </div>
 </body>
 </html>
